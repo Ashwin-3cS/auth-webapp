b708cfe175f313270e401dc26d4fcd3cb2fcad78
Ashwin-3cS <flashashwin2004@gmail.com>
2026-07-19 00:07:06 +0530

    feat: loginform added in the root route instead of having it internally routed

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
 import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import Link from "next/link";
-import { Suspense } from "react";
+import { LoginForm } from "@/components/login-form";
+
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-        </div>
-      </div>
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <LoginForm/>
     </main>
   );
 }
